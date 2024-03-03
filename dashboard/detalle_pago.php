75608cd9b01f703f1a80fad4fdfb26d53cebc13a
Hipol1to <hipolitoprz2001@gmail.com>
2024-03-03 18:55:51 -0400

    demo hide

--- dashboard/detalle_pago.php
+++ dashboard/detalle_pago.php
@@ -676,7 +676,7 @@ $(".delete-btn").click(function() {
 <!-- AdminLTE App -->
 <script src="./dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="./dist/js/demo.js"></script>
+<!-- <script src="./dist/js/demo.js"></script> -->
 <!-- Page specific script -->
 <script>
   $(function () {
--- dashboard/detalle_perfil.php
+++ dashboard/detalle_perfil.php
@@ -444,7 +444,7 @@ if(isset($_GET['id'])) {
 <!-- AdminLTE App -->
 <script src="./dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="./dist/js/demo.js"></script>
+<!-- <script src="./dist/js/demo.js"></script> -->
 <!-- Page specific script -->
 <script>
   $(function () {
